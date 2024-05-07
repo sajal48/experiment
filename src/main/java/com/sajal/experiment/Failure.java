b43a0b6218fe82842cee4ca76f93af62e0a18c1e
os-sajal.halder <os-sajal.halder@rakuten.com>
2024-05-07 15:12:33 +0600

    refactor

--- src/main/java/com/sajal/experiment/Failure.java
+++ src/main/java/com/sajal/experiment/Failure.java
@@ -2,11 +2,10 @@ package com.sajal.experiment;
 
 import java.util.function.Consumer;
 
-// Failure case
-    record Failure<T, E>(E error) implements Either<T, E> {
-        @Override
-        public void fold(Consumer<? super T> successConsumer,
-                         Consumer<? super E> failureConsumer) {
-            failureConsumer.accept(error());
-        }
+record Failure<T, E>(E error) implements Either<T, E> {
+    @Override
+    public void fold(Consumer<? super T> successConsumer,
+                     Consumer<? super E> failureConsumer) {
+        failureConsumer.accept(error());
     }
+}
