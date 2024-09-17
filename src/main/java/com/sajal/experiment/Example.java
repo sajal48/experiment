8435f96d213623a515187bc4eced5b4edaeffff4
os-sajal.halder <os-sajal.halder@rakuten.com>
2024-09-17 11:07:54 +0600

    comment remove

--- src/main/java/com/sajal/experiment/Example.java
+++ src/main/java/com/sajal/experiment/Example.java
@@ -6,29 +6,13 @@ import java.util.Optional;
 
 public class Example {
     public static void main(String[] args) {
-//        UserService userService = new UserService();
-//        Either<User, String> result = userService.getPerson(1);
-//
-//        result.fold(
-//                user -> System.out.println("User: " + user.getName()),
-//                error -> System.out.println("Error: " + error)
-//        );
+        UserService userService = new UserService();
+        Either<User, String> result = userService.getPerson(1);
 
-        Map<String,Object> test = new HashMap<>();
-        Map<String,Object> inventory = new HashMap<>();
-        test.put("variantId","123");
-        test.put("count",1);
-        inventory.put("variantMappedInventories",test);
+        result.fold(
+                user -> System.out.println("User: " + user.getName()),
+                error -> System.out.println("Error: " + error)
+        );
 
-        System.out.println(getQuantity(inventory,"123"));
-
-    }
-    public static int getQuantity(final Map<String, Object> inventory,
-                            final String variantId) {
-
-        return (int) Optional.ofNullable((Map) inventory.get("variantMappedInventories"))
-                .map(it -> (Map) it.get(variantId))
-                .map(it -> it.get("count"))
-                .orElse(0);
     }
 }
